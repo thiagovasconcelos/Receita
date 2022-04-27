267d37bb3764464f280899d48b97fcab9b170908
Aluno <aluno@naverecife.local>
2022-04-26 21:32:35 -0300

    Atualização 1.2

---
+++ ASSETS/desafil.js
@@ -0,0 +1,12 @@
+var t = 10
+for(var i=0;i<t;i++){
+    if(i===0){
+        console.log("f".repeat(t))
+    }else if(i===t/2){
+        console.log("f".repeat(t/2))
+    }
+    console.log("f")
+}
+
+
+
